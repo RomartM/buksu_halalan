cc8292df6ff6446f188eca805fb9be185a32ce08
romart <mediantekid@gmail.com>
2019-03-11 22:12:03 +0800

    Add stylesheet

--- index.php
+++ index.php
@@ -1,12 +1,19 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: romart
- * Date: 3/11/19
- * Time: 6:58 PM
- */
-
-if ($_SERVER["REQUEST_METHOD"]=="GET"){
+    if ($_SERVER["REQUEST_METHOD"]=="GET"){
     $request_page = $_GET["page"];
+?>
+<html lang="en" >
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="assets/css/common.css">
+    <link rel="stylesheet" href="assets/css/<?php echo $request_page;?>.css">
+</head>
+<body>
+<?php
     include "templates/". $request_page .".php";
 }
+?>
+</body>
+</html>
+
+
