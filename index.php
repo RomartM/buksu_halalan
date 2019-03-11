a5a910718ce2fcd281f57cc3a1c34d7a075b828e
romart <mediantekid@gmail.com>
2019-03-11 22:14:42 +0800

    Refactor variable

--- index.php
+++ index.php
@@ -1,19 +1,19 @@
 <?php
     if ($_SERVER["REQUEST_METHOD"]=="GET"){
-        $request_page = $_GET["page"];
+        $request_page_name = $_GET["page"];
 ?>
 <html lang="en" >
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="assets/css/common.css">
-    <link rel="stylesheet" href="assets/css/<?php echo $request_page;?>.css">
+    <link rel="stylesheet" href="assets/css/<?php echo $request_page_name;?>.css">
 </head>
 <body>
 <?php
-    include "templates/". $request_page .".php";
+    include "templates/". $request_page_name .".php";
 ?>
 <script src="assets/js/common.js"></script>
-<script src="assets/js/<?php echo $request_page;?>.js"></script>
+<script src="assets/js/<?php echo $request_page_name;?>.js"></script>
 </body>
 </html>
 <?php } ?>
